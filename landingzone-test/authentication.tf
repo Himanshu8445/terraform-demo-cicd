2d079f257737859106e536bac7ac7f9adac2a88f
Himanshu8445 <himshar@microsoft.com>
2025-11-03 14:15:23 +0530

    re-run

--- landingzone-test/authentication.tf
+++ landingzone-test/authentication.tf
@@ -9,8 +9,8 @@ terraform {
 
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=4.14.0"
+      source           = "hashicorp/azurerm"
+      required_version = "=4.14.0"
     }
   }
 }
