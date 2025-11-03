48862850b1f190483ccfe2c9796cc80fd304559d
Himanshu8445 <himshar@microsoft.com>
2025-11-03 15:10:23 +0530

    re-run

--- landingzone-test/main.tf
+++ landingzone-test/main.tf
@@ -4,9 +4,9 @@ module "resourcegroup" {
 }
 
 module "storage_acct" {
-  source             = "../modules/storageaccount"
-  storage_accounts   = var.storage_accounts
-  containers         = var.containers
+  source           = "../modules/storageaccount"
+  storage_accounts = var.storage_accounts
+  #containers         = var.containers
   sa_additional_tags = var.sa_additional_tags
   depends_on         = [module.resourcegroup]
 }
--- landingzone-test/output.tf
+++ landingzone-test/output.tf
@@ -2,7 +2,6 @@ output "resourcegroup" {
   value     = module.resourcegroup
   sensitive = true
 }
-
 output "storage_acct" {
   value     = module.storage_acct
   sensitive = true
